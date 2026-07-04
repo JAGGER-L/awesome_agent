637af8511c2c1399a2a091b55469a030da5765df
JAGGER-L <1532892732@qq.com>
2026-07-04 13:47:45 +0800

    feat: persist local approval decisions

--- src/awesome_agent/surfaces/local_runtime_container.py
+++ src/awesome_agent/surfaces/local_runtime_container.py
@@ -11,6 +11,7 @@ from awesome_agent.domain.enums import ExecutionOrigin
 from awesome_agent.domain.models import Run
 from awesome_agent.modeling.provider import ModelProvider
 from awesome_agent.persistence.budget import InMemoryBudgetRepository
+from awesome_agent.persistence.local_approvals import LocalApprovalRepository
 from awesome_agent.persistence.local_artifacts import LocalArtifactMetadataRepository
 from awesome_agent.persistence.local_conversations import LocalConversationRepository
 from awesome_agent.persistence.local_dispatch import LocalRunDispatcher
@@ -53,6 +54,7 @@ class LocalRuntimeContainer:
         self.conversations = LocalConversationRepository(database_path)
         self.runtime = LocalRuntimeRepository(database_path)
         self.artifacts = LocalArtifactMetadataRepository(database_path)
+        self.approvals = LocalApprovalRepository(database_path)
         self.dispatcher = LocalRunDispatcher(self.runtime)
         self.events = EventStream()
 
@@ -108,3 +110,4 @@ class LocalRuntimeContainer:
         self.conversations.close()
         self.runtime.close()
         self.artifacts.close()
+        self.approvals.close()
