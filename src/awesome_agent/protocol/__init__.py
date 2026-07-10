743a3bfb033284ac7b9f80d20a3c369d5994535c
JAGGER-L <1532892732@qq.com>
2026-07-11 04:45:48 +0800

    feat(protocol): add json rpc facade adapter

---
+++ src/awesome_agent/protocol/__init__.py
@@ -0,0 +1,15 @@
+from awesome_agent.protocol.jsonrpc import (
+    JSONRPC_VERSION,
+    PROTOCOL_VERSION,
+    JsonRpcDispatcher,
+    event_notification,
+    jsonrpc_error,
+)
+
+__all__ = [
+    "JSONRPC_VERSION",
+    "PROTOCOL_VERSION",
+    "JsonRpcDispatcher",
+    "event_notification",
+    "jsonrpc_error",
+]
